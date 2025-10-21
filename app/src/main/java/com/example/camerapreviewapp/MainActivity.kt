ccab4e609898c10dc3fe13e6786cbb613b522392
Lucas <lucasvida2005@gmail.com>
2025-10-21 17:24:34 -0300

    feat: add permission request

--- app/src/main/java/com/example/camerapreviewapp/MainActivity.kt
+++ app/src/main/java/com/example/camerapreviewapp/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.camerapreviewapp
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.camerapreviewapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied.
+                }
+            }
+
+        binding.btnOpenCamera.setOnClickListener {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
     }
 }
