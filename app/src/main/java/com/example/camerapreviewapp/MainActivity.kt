142b76019df3315994c5ff9c34f7825f39b2f3e2
Lucas <lucasvida2005@gmail.com>
2025-10-21 22:37:53 -0300

    feat: add onActivityResult fun

--- app/src/main/java/com/example/camerapreviewapp/MainActivity.kt
+++ app/src/main/java/com/example/camerapreviewapp/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.camerapreviewapp
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.camerapreviewapp.databinding.ActivityMainBinding
+
+const val CAMERA_REQUEST_CODE = 29
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -25,18 +33,38 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val requestPermissionLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-                if (isGranted) {
-                    // Permission is granted. Continue the action or workflow in your app.
-                } else {
-                    // Explain to the user that the feature is unavailable because the
-                    // feature requires a permission that the user has denied.
-                }
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // feature requires a permission that the user has denied.
             }
+        }
 
         binding.btnOpenCamera.setOnClickListener {
             requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+
+        binding.btnResetPreview.setOnClickListener {
+            // TODO: Implement reset logic for image preview
+        }
+    }
+
+    @Deprecated("Use TakePicturePreview")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.i("CameraPreviewApp", "Image successfully captured.")
+                val result: Bitmap? = data?.getParcelableExtra("data")
+            } else  {
+                Log.i("CameraPreviewApp", "Image capture canceled or failed.")
+            }
         }
     }
 }
