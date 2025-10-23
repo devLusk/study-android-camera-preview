edf5cb57547e9a590c78c7ea1fbc06e27be0781b
Lucas <lucasvida2005@gmail.com>
2025-10-23 15:48:18 -0300

    feat: replace TakePicturePreview  with TakePicture

--- app/src/main/java/com/example/camerapreviewapp/MainActivity.kt
+++ app/src/main/java/com/example/camerapreviewapp/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.example.camerapreviewapp
 
 import android.Manifest
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.camerapreviewapp.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
@@ -25,12 +26,19 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val takePicturePreview = registerForActivityResult(
-            ActivityResultContracts.TakePicturePreview()
-        ) { bitmap: Bitmap? ->
-            if (bitmap != null) {
+        val photoFile = File.createTempFile("photo_", ".jpg", cacheDir)
+        val photoURI = FileProvider.getUriForFile(
+            this,
+            "${applicationContext.packageName}.fileprovider",
+            photoFile
+        )
+
+        val takePicture = registerForActivityResult(
+            ActivityResultContracts.TakePicture()
+        ) { success: Boolean ->
+            if (success) {
                 Log.i("CameraPreviewApp", "Image captured successfully.")
-                binding.imagePreview.setImageBitmap(bitmap)
+                binding.imagePreview.setImageURI(photoURI)
             } else {
                 Log.w("CameraPreviewApp", "No image captured or operation canceled.")
             }
@@ -41,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         ) { isGranted: Boolean ->
             if (isGranted) {
                 Log.i("CameraPreviewApp", "Camera permission granted.")
-                takePicturePreview.launch(null)
+                takePicture.launch(photoURI)
             } else {
                 Log.w("CameraPreviewApp", "Camera permission denied.")
             }
